0fdc85c6748a958ec1b09db1275105d080f60bce
Carsten Skov <carsten@simcax.dk>
2023-10-28 22:36:35 +0200

    Added ping result parsing

--- iconnmon/check_hosts.py
+++ iconnmon/check_hosts.py
@@ -9,31 +9,65 @@ from ipaddress import ip_address
 from shell import shell
 
 
-def _check_ip(value: str) -> bool:
+class IConnMonPing:
     """
-    Validates the str is a valid ip v4 address
+    Utiliy class with ping functionality
     """
-    try:
-        ip = ip_address(value)  # noqa: F841
-        return_value = True
-    except ValueError:
-        return_value = False
-    return return_value
 
+    def _check_ip(self, value: str) -> bool:
+        """
+        Validates the str is a valid ip v4 address
+        """
+        try:
+            ip = ip_address(value)  # noqa: F841
+            return_value = True
+        except ValueError:
+            return_value = False
+        return return_value
 
-def ping_host(host: str, output: str = None) -> bool | list:
-    """
-    Method for pinging a host
-    """
-    result = _check_ip(host)
-    if result:
-        param = "-n" if platform.system().lower() == "windows" else "-c"
+    def ping_host(self, host: str, output: str = None) -> bool | list:
+        """
+        Method for pinging a host
+        """
+        result = self._check_ip(host)
+        if result:
+            param = "-n" if platform.system().lower() == "windows" else "-c"
 
-        command = ["ping", param, "1", host]
-        if output is None:
-            result = subprocess.call(command) == 0
+            command = ["ping", param, "1", host]
+            if output is None:
+                result = subprocess.call(command) == 0
+            else:
+                execute_result = shell(command)
+                result = execute_result.output()
+
+        return result
+
+    def parse_ping_result(self, ping_result: list) -> dict:
+        """
+        Method which parses a ping result, and returns a dict
+        Input: list with 5 lines from the result of a commandline ping
+        Line 1 contains somehting like: 64 bytes from 127.0.0.1: icmp_seq=1 ttl=64 time=0.101 ms
+        on success and:
+        From n.n.n.n icmp_seq=1 Destination Host Unreachable
+        on failure
+        And this is the one we are interested in
+        Output: dict listing the ttl, response_time, ping succeed or not
+        """
+        if isinstance(ping_result, list):
+            the_line = ping_result[1]
+            first_part = the_line.split()[0]
+            response_status = False if first_part == "From" else True
+            ttl = -1 if first_part == "From" else int(the_line.split()[5].split("=")[1])
+            response_time = (
+                -1 if first_part == "From" else float(the_line.split()[6].split("=")[1])
+            )
+
         else:
-            execute_result = shell(command)
-            result = execute_result.output()
-
-    return result
+            response_status = False
+            response_time = -1
+            ttl = -1
+        return {
+            "response_status": response_status,
+            "response_time": response_time,
+            "ttl": ttl,
+        }
--- tests/test_ping.py
+++ tests/test_ping.py
@@ -4,7 +4,7 @@ Tests for pinging and parsing the ping result
 
 import pytest
 
-from iconnmon import check_hosts as ch
+from iconnmon.check_hosts import IConnMonPing
 
 
 def test_ping_localhost():
@@ -12,7 +12,8 @@ def test_ping_localhost():
     Ping localhost and make sure a correct ip address will get a response
     """
     host = "127.0.0.1"
-    result = ch.ping_host(host=host)
+    icm_ping = IConnMonPing()
+    result = icm_ping.ping_host(host=host)
     assert result is True
 
 
@@ -21,7 +22,8 @@ def test_ping_localhost_full_result():
     Ping localhost and receive the ping result
     """
     host = "127.0.0.1"
-    result = ch.ping_host(host=host, output="full")
+    icm_ping = IConnMonPing()
+    result = icm_ping.ping_host(host=host, output="full")
     assert isinstance(result, list)
 
 
@@ -30,12 +32,55 @@ def test_ip_address_failing():
     Test a false when checking a wrong ip v4 address
     """
     address = "127.2222.1.1"
-    assert ch._check_ip(address) is False
+    icm_ping = IConnMonPing()
+    assert icm_ping._check_ip(address) is False
 
 
 def test_ping_localhost_wrong_ip():
     """
     Make sure an invalid IP address will fail
     """
+    icm_ping = IConnMonPing()
     host = "127.0.2222.1"
-    assert ch.ping_host(host=host) is False
+    assert icm_ping.ping_host(host=host) is False
+
+
+def test_parse_ping_response_success():
+    """
+    Test we are able to get ttl, response time and if the ping succeeded
+    """
+    icm_ping = IConnMonPing()
+    host = "127.0.0.1"
+    # result = icm_ping.ping_host(host=host, output="full")
+    result = [
+        "PING 127.0.0.1 (127.0.0.1) 56(84) bytes of data.",
+        "64 bytes from 127.0.0.1: icmp_seq=1 ttl=64 time=0.060 ms",
+        "--- 127.0.0.1 ping statistics ---",
+        "1 packets transmitted, 1 received, 0% packet loss, time 0ms",
+        "rtt min/avg/max/mdev = 0.060/0.060/0.060/0.000 ms",
+    ]
+    parsed_result = icm_ping.parse_ping_result(result)
+    assert isinstance(parsed_result, dict)
+    assert parsed_result.get("ttl") == 64
+    assert parsed_result.get("response_time") == 0.060
+    assert parsed_result.get("response_status")
+
+
+def test_parse_ping_response_failed():
+    """
+    Test we are able to get ttl, response time and if the ping succeeded
+    """
+    icm_ping = IConnMonPing()
+    host = "172.20.20.161"
+    # result = icm_ping.ping_host(host=host, output="full")
+    result = [
+        "PING 10.1.1.1 (10.1.1.1) 56(84) bytes of data.",
+        "From 10.1.1.1 icmp_seq=1 Destination Host Unreachable",
+        "--- 10.1.1.1 ping statistics ---",
+        "1 packets transmitted, 0 received, +1 errors, 100% packet loss, time 0ms",
+    ]
+    parsed_result = icm_ping.parse_ping_result(result)
+    assert isinstance(parsed_result, dict)
+    assert parsed_result.get("ttl") == -1
+    assert parsed_result.get("response_time") == -1
+    assert parsed_result.get("response_status") is False
