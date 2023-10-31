a6a2383754340352aa196a1f51a297e11f049333
Carsten Skov <carsten@simcax.dk>
2023-10-31 21:05:22 +0100

    Added some more influx tests and fixed tests so they actually assert the outcome

--- tests/test_influx.py
+++ tests/test_influx.py
@@ -11,7 +11,40 @@ from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 from shell import shell
 
+from iconnmon.check_hosts import IConnMonPing
 from iconnmon.register import IConnMonnRegister
+
+
+def test_influx_insert_data():
+    """
+    Initial test to add data to influxdb
+    """
+    load_dotenv()
+    token = os.environ.get("INFLUXDB_TOKEN")
+    org = "iconnmon"
+    url = "http://localhost:8086"
+    bucket = "iconnmonseries"
+    iconn_reg = IConnMonnRegister(token=token, org=org, url=url, bucket=bucket)
+    result = iconn_reg.register_point(
+        host="127.0.0.1", ttl=64, response_time=0.101, status=True
+    )
+    assert result
+
+
+def test_influx_insert_data_wrong_token():
+    """
+    Initial test to add data to influxdb
+    """
+    load_dotenv()
+    token = "notTheRealToken"
+    org = "iconnmon"
+    url = "http://localhost:8086"
+    bucket = "iconnmonseries"
+    iconn_reg = IConnMonnRegister(token=token, org=org, url=url, bucket=bucket)
+    result = iconn_reg.register_point(
+        host="127.0.0.1", ttl=64, response_time=0.101, status=True
+    )
+    assert result is False
 
 
 def test_insert_data_in_influx():
@@ -24,18 +57,43 @@ def test_insert_data_in_influx():
     url = "http://localhost:8086"
     bucket = "iconnmonseries"
     iconn_reg = IConnMonnRegister(token=token, org=org, url=url, bucket=bucket)
-
+    outcome = True
     for value in range(50):
-        iconn_reg.register_point(
+        result = iconn_reg.register_point(
             host="127.0.0.1", ttl=64, response_time=0.101, status=True
         )
-        # point = Point("ping").tag("host", "127.0.0.1").field("response", True)
-        # result = write_api.write(bucket=bucket, org=org, record=point,)
-        time.sleep(1)
+        if result:
+            time.sleep(1)
+        else:
+            outcome = False
+    assert outcome
 
 
-def test_retrieve_influx_token():
+def test_add_ping_data_in_influx():
     """
-    Test to retrive a token from influxdb
+    Test to adding data to influxdb
     """
-    pass
+    load_dotenv()
+    token = os.environ.get("INFLUXDB_TOKEN")
+    org = "iconnmon"
+    url = "http://localhost:8086"
+    bucket = "iconnmonseries"
+    iconn_reg = IConnMonnRegister(token=token, org=org, url=url, bucket=bucket)
+    icm_ping = IConnMonPing()
+    host = "127.0.0.1"
+
+    outcome = True
+    for value in range(50):
+        response = icm_ping.ping_host(host=host, output="full")
+        result = icm_ping.parse_ping_result(response)
+        ping_result = iconn_reg.register_point(
+            host=host,
+            status=result.get("response_status"),
+            ttl=result.get("ttl"),
+            response_time=result.get("response_time"),
+        )
+        if ping_result:
+            time.sleep(1)
+        else:
+            outcome = False
+    assert outcome
